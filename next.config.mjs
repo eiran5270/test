180f249eb20998cb72238a3b9cc5f6d6a6a8de3f
이영란 <eiran5270@gmail.com>
2025-01-19 20:30:37 +0900

    v

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",	//추가
 };
 
 export default nextConfig;
--- out/_next/static/Z5AjkNqSNfZN4Rw82LGmI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[{has:[{type:"header",key:"Next-Url",value:"(?:\\/(.*))?[\\/#\\?]?"}],source:"/i/flow/login",destination:"/(.)i/flow/login"}],fallback:[]},"/_error":["static/chunks/pages/_error-7ba65e1336b92748.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
