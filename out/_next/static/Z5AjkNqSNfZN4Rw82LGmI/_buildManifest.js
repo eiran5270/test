71844f79bd8dc0f08342e33bdcea3a9bfd69d1c1
이영란 <eiran5270@gmail.com>
2025-01-19 18:49:50 +0900

    a

---
+++ out/_next/static/Z5AjkNqSNfZN4Rw82LGmI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[{has:[{type:"header",key:"Next-Url",value:"(?:\\/(.*))?[\\/#\\?]?"}],source:"/i/flow/login",destination:"/(.)i/flow/login"}],fallback:[]},"/_error":["static/chunks/pages/_error-7ba65e1336b92748.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
